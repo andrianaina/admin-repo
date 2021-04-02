80c2dafcd94844c3cac8265bdac83650d3fad02f
Henintsoa Andrianaina <andrianainahenintsoa@gmail.com>
2021-04-02 21:45:33 +0300

    make it better

--- server.js
+++ server.js
@@ -1,12 +1,20 @@
-const express=require('express');
-const app=express();
-const path=require('path');
-app.use(express.static(_dirname + '/dist'));
+function requireHTTPS(req, res, next) {
+    // The 'x-forwarded-proto' check is for Heroku
+    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
+        return res.redirect('https://' + req.get('host') + req.url);
+    }
+    next();
+}
+const express = require('express');
+const app = express();
+app.use(requireHTTPS);
 
-app.listen(process.env.port || 8080);
+app.use(express.static('./dist/phoneapp'));
 
-app.get('/*', function(req,res){
-	res.sendFile(path.join(_dirname + '/dist/index.html'));
-})
+app.get('/*', function(req, res) {
+    res.sendFile('index.html', {root: 'dist/phoneapp/'}
+  );
+  });
 
-console.log('Console Listneing');
+  app.listen(process.env.PORT || 8080);
+  console.log("Server is running ...");
