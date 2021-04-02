78864b2a2ff5cac41ef0f84d88998eb13582afb2
Henintsoa Andrianaina <andrianainahenintsoa@gmail.com>
2021-04-03 00:31:19 +0300

    Parametra deploy app 3

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require("express");
 const path =require('path');
 const app = express();
 
-app.use(express.static(_dirname+'/dist/admintelephonie'));
+app.use(express.static(_dirname+'/dist/phoneapp'));
 
 app.get('/*', function(req, res) {
-    res.sendFile(path.join(_dirname+'/dist/admintelephonie/index.html'));
+    res.sendFile(path.join(_dirname+'/dist/phoneapp/index.html'));
 })
   
 app.listen(process.env.PORT);
